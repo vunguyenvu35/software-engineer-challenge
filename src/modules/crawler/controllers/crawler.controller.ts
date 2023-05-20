b1f6199ffc8e2c8453d629cc92a546d1e56d4d8e
Vu Nguyen Vu <vunguyenvu35@gmail.com>
2023-05-20 19:20:26 +0700

    feat: update validate output

--- src/modules/crawler/controllers/crawler.controller.ts
+++ src/modules/crawler/controllers/crawler.controller.ts
@@ -11,6 +11,7 @@ import { CrawlerService } from '../providers';
 import { Request, Response } from 'express';
 import { join } from 'path';
 import { ImageDto } from '../dto';
+import { ScanValidation } from '../validations';
 
 @Controller('crawler')
 export class CrawlerController {
@@ -26,30 +27,31 @@ export class CrawlerController {
 
   @Get('scan')
   public async getImageTable(
-    @Query('url') url: string,
+    @Query() query: ScanValidation,
     @Res({ passthrough: true }) res: Response,
   ) {
-    const page = await this.CrawlerService.fetchData(url);
+    const page = await this.CrawlerService.fetchData(query.url);
     // find table
     const e = await this.CrawlerService.getElementHTML(page.document, 'table');
-
     if (e === null) {
-      return res.json({
+      res.status(403);
+      res.json({
         code: 403,
         message: 'The website has not table element!',
         data: {},
       });
+    } else {
+      const fileName = 'image.png';
+      const image = new ImageDto();
+      image.name = fileName;
+      // make image and response to client.
+      await this.CrawlerService.makeImage(e, image);
+      const file = createReadStream(join(process.cwd(), image.outpath()));
+      res.set({
+        'Content-Type': image.ContentType,
+        'Content-Disposition': 'attachment; filename="' + image.name + '"',
+      });
+      return new StreamableFile(file);
     }
-    const fileName = 'image.png';
-    const image = new ImageDto();
-    image.name = fileName;
-    // make image and response to client.
-    await this.CrawlerService.makeImage(e, image);
-    const file = createReadStream(join(process.cwd(), image.outpath()));
-    res.set({
-      'Content-Type': image.ContentType,
-      'Content-Disposition': 'attachment; filename="' + image.name + '"',
-    });
-    return new StreamableFile(file);
   }
 }
--- src/modules/crawler/crawler.module.ts
+++ src/modules/crawler/crawler.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import * as controllers from './controllers';
 import * as providers from './providers';
 import { ImageDto, PageDto } from './dto';
+import { ScanValidation } from './validations';
 
 @Module({
-  imports: [PageDto, ImageDto],
+  imports: [PageDto, ImageDto, ScanValidation],
   controllers: Object.values(controllers),
   providers: Object.values(providers),
 })
